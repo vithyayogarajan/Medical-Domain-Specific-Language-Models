ca07346b613132f0ef79ad55108584ea7b0a634a
Vithya Yogarajan <60803118+vithyayogarajan@users.noreply.github.com>
2021-09-08 14:26:43 +1200

    Update WEKA_script_log.sh

--- Binary_classification/WEKA_script_log.sh
+++ Binary_classification/WEKA_script_log.sh
@@ -1,2 +1,2 @@
-java -cp weka-3-8-0-monolithic.jar -Xmx16G weka.classifiers.functions.Logistic -t text.arff  -R 1.0 > Logistic_text_1.txt ;
+java -cp weka-3-8-0-monolithic.jar -Xmx16G weka.classifiers.functions.Logistic -t T300features-binary-circ.arff  -R 1.0 > Logistic_text_1.txt ;
 
