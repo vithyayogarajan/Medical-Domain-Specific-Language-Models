8180bf710bd964a3677918f96e879c4f6c09aefd
Vithya Yogarajan <60803118+vithyayogarajan@users.noreply.github.com>
2021-09-08 18:12:10 +1200

    Update script_meka_ml.sh

--- Multilabel/script_meka_ml.sh
+++ Multilabel/script_meka_ml.sh
@@ -1,4 +1,4 @@
-java -Xmx16G -cp "./lib/*" meka.classifiers.multilabel.BR -t input.arff -x 10 -verbosity 4 -W weka.classifiers.functions.Logistic -- -R 1  > output.txt ;
+java -Xmx16G -cp "./lib/*" meka.classifiers.multilabel.BR -t input_multilabel_18labels.arff -x 10 -verbosity 4 -W weka.classifiers.functions.Logistic -- -R 1  > output.txt ;
 
-java -Xmx16G -cp "./lib/*" meka.classifiers.multilabel.meta.BaggingML -W meka.classifiers.multilabel.CC -verbosity 4 -t input.arff -- -S 0 -W weka.classifiers.functions.Logistic -- -R 1.0 > output.txt ;
+java -Xmx16G -cp "./lib/*" meka.classifiers.multilabel.meta.BaggingML -W meka.classifiers.multilabel.CC -verbosity 4 -t input_multilabel_18labels.arff -- -S 0 -W weka.classifiers.functions.Logistic -- -R 1.0 > output.txt ;
 
